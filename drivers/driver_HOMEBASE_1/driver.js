300c14dcbdc2009f3bc9b3d601edd8e6f8b6a5e1
Martijn Poppen <martijn.poppen@kpn.com>
2021-03-30 20:44:11 +0200

    FIX: homebase 1 pairing issue

--- drivers/driver_HOMEBASE_1/driver.js
+++ drivers/driver_HOMEBASE_1/driver.js
@@ -1,7 +1,7 @@
 const mainHubDriver = require('../main-hub-driver');
 const { DEVICE_TYPES } = require('../../constants/device_types');
 
-module.exports = class driver_HOMEBASE extends mainHubDriver {
+module.exports = class driver_HOMEBASE_1 extends mainHubDriver {
     deviceType() {
         return DEVICE_TYPES.HOMEBASE_1
     }
