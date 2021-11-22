7fe7dba75a14d544f361ae25fda96856ce06fbcb
Martijn Poppen <m.poppen@ziggo.nl>
2021-11-22 17:48:43 +0100

    NEW: add Solocam Series

---
+++ drivers/driver_SOLOCAM_S2040/driver.js
@@ -0,0 +1,9 @@
+const mainDriver = require('../main-driver');
+const { DEVICE_TYPES } = require('../../../constants/device_types');
+
+
+module.exports = class driver_SOLOCAM_S2040 extends mainDriver {
+    deviceType() {
+        return DEVICE_TYPES.SOLOCAM_S2040
+    }
+}
