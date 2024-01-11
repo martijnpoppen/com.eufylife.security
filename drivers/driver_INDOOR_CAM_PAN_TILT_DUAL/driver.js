7872394b4b68d9ea69d7c921ca56fd17215e0642
Martijn Poppen <m.poppen@ziggo.nl>
2024-01-11 09:39:41 +0100

    FIX: c210 flowcards

--- drivers/driver_INDOOR_CAM_PAN_TILT_DUAL/driver.js
+++ drivers/driver_INDOOR_CAM_PAN_TILT_DUAL/driver.js
@@ -3,6 +3,6 @@ const mainDriver = require('../main-driver');
 
 module.exports = class driver_INDOOR_CAM_PAN_TILT extends mainDriver {
     deviceType() {
-        return this.homey.app.deviceTypes.INDOOR_CAM_PAN_TILT
+        return this.homey.app.deviceTypes.INDOOR_CAM_PAN_TILT_DUAL
     }
 }
