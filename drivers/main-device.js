5b613bb40ee9a204b0a9a4e31ef362da06adffac
Martijn Poppen <m.poppen@ziggo.nl>
2025-04-16 15:41:42 +0200

    NEW: IndoorCam P&T E30 - (T8417)

--- drivers/main-device.js
+++ drivers/main-device.js
@@ -398,9 +398,13 @@ module.exports = class mainDevice extends Homey.Device {
         try {
             this.homey.app.log(`[Device] ${this.getName()} - onCapability_CMD_IRCUT_SWITCH - `, value);
 
-            await this.homey.app.eufyClient.setDeviceProperty(this.HomeyDevice.device_sn, PropertyName.DeviceNightvision, value);
-            await this.homey.app.eufyClient.setDeviceProperty(this.HomeyDevice.device_sn, PropertyName.DeviceAutoNightvision, value);            
-
+            
+            if(this.EufyDevice.hasProperty(PropertyName.DeviceNightvision)) {
+                await this.homey.app.eufyClient.setDeviceProperty(this.HomeyDevice.device_sn, PropertyName.DeviceNightvision, value);
+            } else {
+                await this.homey.app.eufyClient.setDeviceProperty(this.HomeyDevice.device_sn, PropertyName.DeviceAutoNightvision, value);
+            }
+            
             return Promise.resolve(true);
         } catch (e) {
             this.homey.app.error(e);
